6a9cbf01b3e3f03dcc4a56d1517314fed023b643
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-01-17 12:00:52 +0500

    Feat: notes module (get)
    - API handling for notes (reducer, actions and requests)
    - API constants added for notes
    - Custom components added

---
+++ src/store/actions/note-action.js
@@ -0,0 +1,41 @@
+import { requestHeaders } from '@/utils/helpers'
+import ApiResource from '../../services/api'
+import ApiConstants from '../../services/api-constants'
+
+async function createNote(payload, thunkAPI) {
+    try {
+        const response = await ApiResource.post(ApiConstants.createNote, payload?.body, requestHeaders(payload?.token))
+
+        return response
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+}
+
+async function getAllNotes(payload, thunkAPI) {
+    try {
+        if(!payload?.course_id) return
+        const response = await ApiResource.get(`${ApiConstants.getNotes}/${payload?.course_id}`, requestHeaders(payload?.token))
+
+        return response
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+}
+
+async function deleteNote(payload, thunkAPI) {
+    try {
+        if(!payload?.note_id) return;
+        const response = await ApiResource.delete(`${ApiConstants.deleteNote}/${payload?.note_id}`, requestHeaders(payload?.token))
+        return response
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+}
+
+
+export const NoteApiServices = {
+    createNote,
+    getAllNotes,
+    deleteNote,
+}
