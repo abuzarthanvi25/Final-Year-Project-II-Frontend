a689c6dbed37a2150e4bab25e658d17a2084b84a
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-02-01 19:10:48 +0500

    Feat: UI fixes and enhancement

--- src/pages/dashboard/profile.jsx
+++ src/pages/dashboard/profile.jsx
@@ -133,8 +133,8 @@ export function Profile() {
 
   return (
     <>
-      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url('/img/background-image.png')] bg-cover	bg-center">
-        <div className="absolute inset-0 h-full w-full bg-gray-900/75" />
+      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url('/img/background-image.png')] bg-contain	bg-center">
+        <div className="absolute inset-0 h-full w-full bg-gray-900/20" />
       </div>
       <Card className="mx-3 -mt-16 mb-6 lg:mx-4 border border-blue-gray-100">
         <CardBody className="p-4">
