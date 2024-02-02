d30c61d5772a6a189be11e653d97f1d187feb49f
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-02-02 12:56:58 +0500

    Feat: Dashboard Redesign
    - Modified UI for dashboard

--- src/data/statistics-cards-data.js
+++ src/data/statistics-cards-data.js
@@ -1,54 +1,34 @@
 import {
-  BanknotesIcon,
-  UserPlusIcon,
-  UsersIcon,
-  ChartBarIcon,
+  UserGroupIcon,
+  UserIcon,
+  DocumentIcon,
+  RectangleGroupIcon
 } from "@heroicons/react/24/solid";
 
 export const statisticsCardsData = [
   {
     color: "gray",
-    icon: BanknotesIcon,
-    title: "Assignments Completed",
+    icon: UserIcon,
+    title: "Personal Courses",
     value: "13",
-    footer: {
-      color: "text-green-500",
-      value: "+55%",
-      label: "than last week",
-    },
   },
   {
     color: "gray",
-    icon: UsersIcon,
-    title: "Assignments Pending",
+    icon: UserGroupIcon,
+    title: "Group Courses",
     value: "5",
-    footer: {
-      color: "text-green-500",
-      value: "+3%",
-      label: "than last month",
-    },
   },
   {
     color: "gray",
-    icon: UserPlusIcon,
+    icon: DocumentIcon,
     title: "Total Notes",
     value: "462",
-    footer: {
-      color: "text-red-500",
-      value: "-2%",
-      label: "than yesterday",
-    },
   },
   {
     color: "gray",
-    icon: ChartBarIcon,
+    icon: RectangleGroupIcon,
     title: "Total Summaries",
     value: "33",
-    footer: {
-      color: "text-green-500",
-      value: "+5%",
-      label: "than yesterday",
-    },
   },
 ];
 
